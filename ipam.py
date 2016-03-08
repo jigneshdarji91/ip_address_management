2dd60e87c950ae3290b813f111b083d906f1d850
Jignesh Darji <jigneshdarji.91@gmail.com>
2016-03-07 21:21:38 -0500

    basic allocation done

--- ipam.py
+++ ipam.py
@@ -18,30 +18,51 @@ log.getLogger().addHandler(ch)
 
 class IPAddressManager:
 
-    network = ipaddress.IPv4Network("0.0.0.0")
-
     def __init__(self, network_):
         log.debug("started network: " + network_)
         self.network = ipaddress.IPv4Network(network_)
+        self.allocated = []
         self.unallocated = [self.network]
+        self.waiting = []
 
     def add_network(self, size):
         log.debug("begin")
-        # 1. parse the unallocated list for a network with sufficient space
-        self.get_prefix_for_size(size)
+        prefix = self.get_prefix_for_size(size)
+        flag = False
         for test_network in self.unallocated:
-            log.debug("network: " + str(test_network) + " has hosts: " + str(test_network.numhosts))
-            if test_network.numhosts > size:
-                log.debug("network " + str(test_network) + " has sufficient space")
+            log.debug("network: " + str(test_network) + " has prefix: " + str(test_network.prefixlen))
+            if test_network.prefixlen <= prefix:
+                prefix_diff = int(prefix - test_network.prefixlen)
+                log.debug("network " + str(test_network) + " prefix_diff: " + str(prefix_diff))
+                split_network = test_network.subnet(prefixlen_diff=prefix_diff)
+                log.info("Allocated: " + str(split_network[0]))
+                self.allocated.append(split_network[0])
+                self.unallocated.remove(test_network)
+                for i in range(1, prefix_diff + 1):
+                    self.unallocated.append((test_network.subnet(prefixlen_diff=i))[1])
+                flag = True
+                break
 
-        # 2. break the network that was used to allocate
+        if flag is False:
+            log.info("insufficient address space")
+            self.waiting.append(size)
+
+        print "====Unallocated===="
+        for netw in self.unallocated:
+            print netw
+        print "====Allocated======"
+        for netw in self.allocated:
+            print netw
+        print "====Waiting========"
+        for size in self.waiting:
+            print size
 
     def get_prefix_for_size(self, size):
         prefix = 32 - math.ceil(math.log(size, 2))
         log.debug("size: %d prefix length: %d", size, prefix)
+        return prefix
 
-ipam = IPAddressManager("192.168.0.0/24")
+ipam = IPAddressManager("192.168.0.0/23")
+ipam.add_network(250)
 ipam.add_network(256)
-ipam.add_network(512)
-ipam.add_network(1024)
-ipam.add_network(8000)
+ipam.add_network(256)
